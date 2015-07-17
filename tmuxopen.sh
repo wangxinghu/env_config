5941587a934d58bc9a31d775f9eaab8f91094d35
王兴虎 <wangxinghu@wangxinghudeMacBook-Pro.local>
2015-07-17 19:19:59 +0800

    update

--- tmuxopen.sh
+++ tmuxopen.sh
@@ -12,26 +12,43 @@ $cmd has -t $session 2> /dev/null
 
 if [ $? != 0 ]; then
     $cmd new -d -n sourcecode -s $session
+    $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/st/sourcecode/' C-m
     $cmd neww -n github -t $session
+    $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/st/github/' C-m
     $cmd neww -n work -t $session
+    $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/work/' C-m
     $cmd neww -n log -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/work/farm/log/ffs/' C-m
     $cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/ffsserverwf.log' C-m
     $cmd splitw -h -p 50 -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/work/farm/log/ffs/' C-m
     $cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/error.txt' C-m
     $cmd neww -n mysql -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "mysql -uroot -h127.0.0.1" C-m
     $cmd send-keys -t $session "use farm_1" C-m
     $cmd neww -n mongo -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "mongo" C-m
     $cmd send-keys -t $session "use farm_1" C-m
     $cmd neww -n testvm -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "testvm" C-m
     $cmd neww -n testflight -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "testflight" C-m
     $cmd neww -n other -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd selectw -t $session:1
     #$cmd neww -n mysql -t $session "mysql -uroot -h127.0.0.1"
 fi
