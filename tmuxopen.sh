adf6fb4b9d48ec2a605fd39a737c1b86f1ca32ec
王兴虎 <wangxinghu@wangxinghudeMacBook-Pro.local>
2015-08-20 14:35:10 +0800

    c1,c2,c3

--- tmuxopen.sh
+++ tmuxopen.sh
@@ -20,20 +20,23 @@ if [ $? != 0 ]; then
     $cmd neww -n work -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/work/' C-m
+    $cmd neww -n work_server -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/work/' C-m
     $cmd neww -n log -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/work/farm/log/ffs/' C-m
-    $cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/ffsserverwf.log' C-m
+    #$cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/ffsserverwf.log' C-m
     $cmd splitw -h -p 50 -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/work/farm/log/ffs/' C-m
-    $cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/error.txt' C-m
-    $cmd neww -n mysql -t $session
+    #$cmd send-keys -t $session 'tail -f ~/work/farm/log/ffs/error.txt' C-m
+    $cmd neww -n db -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "mysql -uroot -h127.0.0.1" C-m
     $cmd send-keys -t $session "use farm_1" C-m
-    $cmd neww -n mongo -t $session
+    $cmd splitw -h -p 50 -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/' C-m
     $cmd send-keys -t $session "mongo" C-m
@@ -41,11 +44,17 @@ if [ $? != 0 ]; then
     $cmd neww -n testvm -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/' C-m
-    $cmd send-keys -t $session "testvm" C-m
-    $cmd neww -n testflight -t $session
+    #$cmd send-keys -t $session "testvm" C-m
+    $cmd splitw -h -p 50 -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/' C-m
-    $cmd send-keys -t $session "testflight" C-m
+    #$cmd send-keys -t $session "testflight" C-m
+    $cmd neww -n online_log -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
+    $cmd splitw -h -p 50 -t $session
+    $cmd send-keys -t $session "zsh" C-m
+    $cmd send-keys -t $session 'cd ~/' C-m
     $cmd neww -n other -t $session
     $cmd send-keys -t $session "zsh" C-m
     $cmd send-keys -t $session 'cd ~/' C-m
